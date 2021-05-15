1e39d3f43113c213b71fe11ee791c3986aabba60
Teemamin <teemamin@yahoo.com>
2021-05-15 22:01:34 +0400

    added votes sorting functionality,checking for duplicates before adding API data to to state, try/catch incase of network error

--- src/JokeList.js
+++ src/JokeList.js
@@ -18,6 +18,10 @@ class JokeList extends Component{
         jokes : JSON.parse(window.localStorage.getItem("jokes") || "[]"),
         isLoading : false
     }
+    this.seenJokes = new Set(this.state.jokes.map(j => j.joke))
+    // this.t = this.state.jokes.map(j => j.joke)
+    // console.log(this.seenJokes.has("What do you get hanging from Apple trees? Sore arms."))
+    // console.log(this.seenJokes)
     this.getJokes = this.getJokes.bind(this)
   }
 
@@ -27,23 +31,34 @@ class JokeList extends Component{
 
 
   async getJokes(){
-    let collectedJokes = []
-    while(collectedJokes.length < this.props.numofdefaultjokes){
-        let res = await axios.get('https://icanhazdadjoke.com/', 
-        {headers:{Accept: "application/json"}})
-
-        collectedJokes.push({id:uuidv4(),joke:res.data.joke,votes:0})
+      try{
+        let collectedJokes = []
+        while(collectedJokes.length < this.props.numofdefaultjokes){
+            let res = await axios.get('https://icanhazdadjoke.com/', 
+            {headers:{Accept: "application/json"}})
+            let newJoke = res.data.joke;
+            //if the new jokes collected from the API dnt exixit in seenjokes set add the new jokes
+            if(!this.seenJokes.has(newJoke)){
+                collectedJokes.push({id:uuidv4(),joke:res.data.joke,votes:0})
+            }else{
+                console.log("dup found")
+                console.log(newJoke)
+            }
+        }
+    //get n copy all the prevstate and  jokes gotn bck frm the APi and update the current state with both
+    // combine them both into a new array
+        this.setState(prevState=>({
+            jokes:[...prevState.jokes, ...collectedJokes],
+            isLoading : false
+            
+        })
+        //it will wait till state is done being set, then set the local storage
+        ,()=>window.localStorage.setItem("jokes",JSON.stringify(this.state.jokes))
+        )
+    }catch(e){
+        alert(e);
+        this.setState({isLoading:false})
     }
-//get n copy all the prevstate and  jokes gotn bck frm the APi and update the current state with both
-// combine them both into a new array
-    this.setState(prevState=>({
-        jokes:[...prevState.jokes, ...collectedJokes],
-        isLoading : false
-        
-    })
-    //it will wait till state is done being set, then set the local storage
-    ,()=>window.localStorage.setItem("jokes",JSON.stringify(this.state.jokes))
-    )
  
   }
 
@@ -72,7 +87,9 @@ handleAddJokes =()=>{
             </div>
         )
     }
-      let jokes = this.state.jokes.map((joke,i)=>{
+    //providing a compare function to sort()wil compare the two values ans sort accoridngly
+     let sortedJokes = this.state.jokes.sort((a,b)=> b.votes - a.votes)
+      let jokes = sortedJokes.map((joke,i)=>{
           return <Joke joke={joke.joke} key={i} votes={joke.votes} id={joke.id}
             upvote={()=>this.handlevote(joke.id,1)}
             downvote={()=>this.handlevote(joke.id,-1)}
